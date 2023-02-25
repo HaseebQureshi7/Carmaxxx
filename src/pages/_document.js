eaa95bcdb47f830742255d5f9e6e4f7671cefda8
Haseeb <qureshihaxeeb2@gmail.com>
2023-02-25 14:25:49 +0530

    Async script added!

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,8 +8,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
 
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" async></script>
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
       </Head>
       <body>
         <Main />
